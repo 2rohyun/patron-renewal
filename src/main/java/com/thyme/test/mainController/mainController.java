8ba30da6b8f3e9c1b6e33e85ac13c3c8c28d2f5e
Ryu <yjryou@nodamen.com>
2021-06-10 10:10:16 +0900

    test branch
    test branch

---
+++ src/main/java/com/thyme/test/mainController/mainController.java
@@ -0,0 +1,19 @@
+package com.thyme.test.mainController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mainController {
+
+    @GetMapping("/")
+    public String index(Model model){
+
+        model.addAttribute("hello", "hi");
+        return "index";
+    }
+
+
+
+}
