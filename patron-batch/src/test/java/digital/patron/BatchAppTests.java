a3119ef51a9dde456e6da49a964413138b49702a
2rohyun <dlehgus5656@naver.com>
2021-08-25 15:55:14 +0900

    delete code-deploy-patron-batch file

--- patron-batch/src/test/java/digital/patron/BatchAppTests.java
+++ patron-batch/src/test/java/digital/patron/BatchAppTests.java
@@ -2,13 +2,19 @@ package digital.patron;
 
 import digital.patron.repository.ArtworkRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBatchTest
+@EnableBatchProcessing
+@Configuration
 public class BatchAppTests {
 
-    @Test
-    void contextLoads() {
+    @Bean
+    public JobLauncherTestUtils jobLauncherTestUtils() {
+        return new JobLauncherTestUtils();
     }
 }
